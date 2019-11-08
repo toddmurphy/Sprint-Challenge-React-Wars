271dc3773d355051d82eb8134b4285bde78f5f5c
toddmurphy <toddmurphy93@gmail.com>
2019-11-08 15:02:41 -0400

    added images and titles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -10,11 +10,29 @@ const AppContainer = styled.div`
 const AppTitle = styled.div`
   font-size: 3rem;
   text-align: center;
+  padding: 5% 0;
 `;
 
-// const MovieImage = styled.img`
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5% 0;
 
-// `
+  border: 2px solid green;
+`;
+
+const HeaderTitle = styled.div`
+  border: 1px solid blue;
+  width: 30%;
+  font-size: 3rem;
+  margin: 0 2%;
+  display: flex;
+  align-items: center;
+`;
+
+const MovieImage = styled.img`
+  width: 50%;
+`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -26,11 +44,11 @@ const App = () => {
   return (
     <AppContainer>
       <AppTitle>React Wars: Ready for Battle </AppTitle>
-      <div>
-        <img src="https://images.unsplash.com/photo-1547700055-b61cacebece9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
-      </div>
-
-      <CharacterList />
+      <HeaderContainer>
+        <HeaderTitle>May the Lambda Force be with you...Always!</HeaderTitle>
+        <MovieImage src="https://images.unsplash.com/photo-1547700055-b61cacebece9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+      </HeaderContainer>
+      <CharacterList /> //Component
     </AppContainer>
   );
 };
